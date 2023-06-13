a9301b39db927d3878359588617592d2a5351cfe
Frank Jia <frankjzy@gmail.com>
2023-06-13 16:28:58 -0500

    fixed dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <body className="bg-gray-base font-normal text-[rem] text-black-base dark:bg-gray-inverted dark:text-black-inverted">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
